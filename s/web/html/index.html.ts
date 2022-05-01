91f8d27b1b0e2961e669656109a4f29c681be95a
Chase Moskal <chasemoskal@gmail.com>
2022-04-30 20:18:50 -0700

    improve: website content, links

--- s/web/html/index.html.ts
+++ s/web/html/index.html.ts
@@ -3,12 +3,35 @@ import {MyWebsiteContext} from "../types.js"
 import pageHtml from "./partials/page.html.js"
 import {html} from "xiome/x/toolbox/hamster-html/html.js"
 
-export default (context: MyWebsiteContext) => pageHtml({
+const links = {
+	"xiome": "https://github.com/chase-moskal/xiome",
+	"benevolent.games": "https://github.com/benevolent-bees/benevolent.games",
+	"webp-hero": "https://github.com/chase-moskal/webp-hero",
+	"sparrow-rtc": "https://github.com/chase-moskal/sparrow-rtc",
+	"importly": "https://github.com/chase-moskal/importly",
+	"snapstate": "https://github.com/chase-moskal/snapstate",
+	"dbmage": "https://github.com/chase-moskal/dbmage",
+	"shopper": "https://github.com/chase-moskal/shopper",
+	"crnc": "https://github.com/chase-moskal/crnc",
+	"renraku": "https://github.com/chase-moskal/renraku",
+}
+
+export default ({v, ...context}: MyWebsiteContext) => pageHtml({
 	title: "chasemoskal.com",
-	context,
+	context: {v, ...context},
 	mainContent: html`
-		<h1>chase moskal.</h1>
-		<p>too busy on github to have a cool website.</p>
-		<p data-indent><a href="https://github.com/chase-moskal">@chase-moskal</a></p>
+		<h1>
+			<img alt="" src="${v("/assets/chase.jpg")}"/>
+			<span>chase moskal</span>
+		</h1>
+		
+		<p>"i'm too busy on github to have a cool website"</p>
+		<p><a href="https://github.com/chase-moskal">github.com/chase-moskal</a></p>
+
+		<ul>
+			${Object.entries(links).map(([label, href]) => html`
+				<li><a href="${href}">${label}</a></li>
+			`)}
+		</ul>
 	`
 })
